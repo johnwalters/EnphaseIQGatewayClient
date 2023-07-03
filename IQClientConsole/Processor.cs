f758495b4d303bae144a26096c7123a3eae2022c
John Walters <john.walters.92117@gmail.com>
2023-07-03 15:13:46 -0700

    Added Console app to fetch data periodically

---
+++ IQClientConsole/Processor.cs
@@ -0,0 +1,101 @@
+﻿using IQClientLib;
+using IQClientLib.Database.Models;
+using Microsoft.Extensions.Configuration;
+
+
+namespace IQClientConsole
+{
+    public class Processor
+    {
+        private IConfiguration _config;
+        private Client _client;
+
+        public Processor()
+        {
+            _config = InitConfiguration();
+            var token = _config["Token"];
+            var connectionString = _config["ConnectionStrings:DefaultConnection"];
+            _client = new Client(token, connectionString);
+        }
+
+        public void GetIQResponses()
+        {
+            WriteMessage("Getting Inverters response.");
+            var inverterResponse = _client.GetInverters().Result;
+            Console.WriteLine(new IQResponse(inverterResponse).JsonData + "\n");
+
+            WriteMessage("Getting Meters response.");
+            var meterResponse = _client.GetMeters().Result;
+            Console.WriteLine(new IQResponse(meterResponse).JsonData + "\n");
+
+            WriteMessage("Getting Meter Readings response.");
+            var meterReadingResponse = _client.GetMeterReadings().Result;
+            Console.WriteLine(new IQResponse(meterReadingResponse).JsonData + "\n");
+
+            WriteMessage("Getting Status response.");
+            var statusResponse = _client.GetStatus().Result;
+            Console.WriteLine(new IQResponse(statusResponse).JsonData + "\n");
+
+            WriteMessage("Getting Consumption response.");
+            var consumptionResponse = _client.GetConsumption().Result; 
+            Console.WriteLine(new IQResponse(consumptionResponse).JsonData + "\n");
+        }
+
+        public void GetInverters()
+        {
+            WriteMessage("Getting Inverters response.");
+            var inverterResponse = _client.GetInverters().Result;
+            Console.WriteLine(new IQResponse(inverterResponse).JsonData + "\n");
+
+           
+        }
+
+        public void GetMeters()
+        {
+            WriteMessage("Getting Meters response.");
+            var meterResponse = _client.GetMeters().Result;
+            Console.WriteLine(new IQResponse(meterResponse).JsonData + "\n");
+
+        }
+
+        public void GetMeterReadings()
+        {
+            WriteMessage("Getting Meter Readings response.");
+            var meterReadingResponse = _client.GetMeterReadings().Result;
+            Console.WriteLine(new IQResponse(meterReadingResponse).JsonData + "\n");
+
+        }
+
+        public void GetStatus()
+        {
+
+            WriteMessage("Getting Status response.");
+            var statusResponse = _client.GetStatus().Result;
+            Console.WriteLine(new IQResponse(statusResponse).JsonData + "\n");
+        }
+
+        public void GetConsumption()
+        {
+            WriteMessage("Getting Consumption response.");
+            var consumptionResponse = _client.GetConsumption().Result;
+            Console.WriteLine(new IQResponse(consumptionResponse).JsonData + "\n");
+        }
+
+        private void WriteMessage(string message, bool addPressCtrlCToCancelMessage = false)
+        {
+            Console.WriteLine(DateTime.Now.ToString("y-MM-dd hh:mm:ss") + " " + message);
+            if (!addPressCtrlCToCancelMessage) return;
+            Console.WriteLine("Press ctrl+C to cancel.");
+        }
+
+        private IConfiguration InitConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile("appsettings.Development.json")
+            //.AddJsonFile("appsettings.Production.json")
+            .Build();
+            return config;
+        }
+    }
+}
---
+++ IQClientConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace IQClientConsole
+{
+    internal class Program
+    {
+        private static readonly int _waitBetweenAllRequestsSeconds = 300;
+        private static readonly int _waitBetweenEachRequestSeconds = 10;
+
+
+        static void Main(string[] args)
+        {
+            var processor = new Processor();
+            var done = false;
+            while (!done)
+            {
+                processor.GetInverters();
+                Thread.Sleep(1000 * _waitBetweenEachRequestSeconds);
+                processor.GetMeters();
+                Thread.Sleep(1000 * _waitBetweenEachRequestSeconds);
+                processor.GetMeterReadings();
+                Thread.Sleep(1000 * _waitBetweenEachRequestSeconds);
+                processor.GetStatus();
+                Thread.Sleep(1000 * _waitBetweenEachRequestSeconds);
+                processor.GetConsumption();
+
+                Console.WriteLine(DateTime.Now.ToString("y-MM-dd hh:mm:ss") + $" Waiting {_waitBetweenAllRequestsSeconds} seconds ...") ;
+                Console.WriteLine("Press ctrl+C to cancel.");
+                Thread.Sleep(1000 * _waitBetweenAllRequestsSeconds);
+            }
+            
+        }
+
+        
+    }
+}
